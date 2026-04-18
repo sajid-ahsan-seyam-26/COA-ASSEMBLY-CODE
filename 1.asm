4e2149aaa2a4b7e066fcc7efb4b6bdbf8ec1a8ca
sajid-ahsan-seyam-26 <sajidahsanseyamstudent@gmail.com>
2026-04-18 22:55:47 +0600

    upload 1 code

---
+++ 1.asm
@@ -0,0 +1,21 @@
+.MODEL SMALL
+.STACK 100H
+.DATA
+.CODE
+MAIN PROC
+    MOV AX, @DATA
+    MOV DS, AX
+
+    MOV AH, 2
+    MOV DL, 'A'
+
+PRINT_LOOP:
+    INT 21H
+    INC DL
+    CMP DL, '['
+    JNE PRINT_LOOP
+
+    MOV AH, 4CH
+    INT 21H
+MAIN ENDP
+END MAIN
